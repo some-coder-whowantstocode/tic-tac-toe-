21f3450a2cf85b61aab2a9b7cb69c4478db8675d
Rohit <rohit19003@gmail.com>
2023-05-10 00:03:46 +0530

    updating game

--- index.js
+++ index.js
@@ -1,68 +1,184 @@
-let turn = "x"
-let next = document.querySelector("#info")
-const reset = document.querySelector("#reset")
-let gameover = false;
-
-const changeturn =()=>{
-  
-    return turn =  turn== "x"?"0": "x"
-}
-
-const win = () =>{
-    let b =Array.from(document.getElementsByClassName("boxtext"))
-    let wins = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
-    wins.forEach(e =>{
-        if((b[e[0]].innerText===b[e[1]].innerText)&&(b[e[1]].innerText===b[e[2]].innerText)&&(b[e[0]].innerText!=='')){
-            next.innerText = b[e[0]].innerText+" is winner"
-            gameover=true
-        }
-    })
-}
+const block =Array.from(document.querySelectorAll(".block"))
+const reset = document.querySelector(".reset")
+const result = document.querySelector(".result")
+const singleplayer = document.querySelector(".singleplayer")
+const multiplayer = document.querySelector(".multiplayer")
+const turn = document.querySelector(".turn")
 
 
-let boxes =Array.from(document.querySelectorAll(".box"))
+let text ='x';
+let winner = false;
+let auto = false;
+let leftover = true;
+let target ;
+let botturn = true;
 
-boxes.forEach(element => {
-    let boxtext = element.querySelector(".boxtext")
-    element.addEventListener("click",
-     function(){
-        if(boxtext.innerText === ''){
-            boxtext.innerText = turn;
-            changeturn();
-            // console.log(gameover)
 
-            win();
-            if(!gameover){
-                next.innerText= "turn for "+turn
+result.innerHTML = 'turn for : '+ text
+singleplayer.addEventListener("click",
+function(){
+    auto = false;
+    change()
+})
+
+multiplayer.addEventListener("click",
+function(){
+    auto = true;
+    result.innerHTML=''
+    change()
+})
+
+block.forEach(e=>{
+    e.addEventListener("click",
+    function(){
+        if(auto == false){
+            if(e.innerHTML==' ' && winner == false ){
+               
+                e.innerHTML=text;
+                
+                for(let c of combination){
+                    if(checkwin(c)){
+                        result.innerHTML= text + " is the winner";
+                        break;
+                    }
+                   }
+                   text == 'x' ? text = "0" : text = 'x'
+                result.innerHTML = 'turn for : '+ text
+
             }
-           
         }
+       else{
+        if(text == 'x'){
+            if(e.innerHTML===' ' && winner == false ){
+                e.innerHTML=text;
+                
+                target = block.indexOf(e);
+                for(let c of combination){
+                    if(checkwin(c)){
+                        result.innerHTML= text + " is the winner";
+                        break;
+                    }
+                   }
+                   console.log(text)
+
+                   text == 'x' ? text = "0" : text = 'x'
+                   console.log(text)
+                   leftover=true
+                  if(winner == false){
+                    botturn = true;
+                    bot()
+                  
+                  }
+            }
+        }
+       
+    }
         
+    
+       
+       
     })
 })
 
+let arr =[]
 
+const bot =()=>{
 
-reset.addEventListener(
-    "click",
-    function() {
-        let boxtexts = Array.from(document.querySelectorAll(".boxtext"))
-        boxtexts.forEach(e => {
-            e.innerText=''
-        })
-        turn="x";
-        gameover=false;
-        next.innerText="turn for x"
+    block.forEach((e)=>{
+        if(e.innerHTML == ' '){
+            if(leftover===true){
+            
+                
+                for(let c of combination){
+                  
+                    if(check(c)){
+                        break;
+                    }
+                   }
+             
+            
+                leftover=false
+            }
+       
+        }
+    })
     }
-)
 
 
+    const check = (indexes) => {
+        indexes.every(i => {
+          if (i === target && botturn == true) {
+            for (let e of indexes) {
+              if (block[e].innerHTML === " ") {
+                block[e].innerHTML = text;
+                botturn = false;
+                for (let c of combination) {
+                  if (checkwin(c)) {
+                    result.innerHTML = text + " is the winner";
+                   
+                 
+                  }
+                }
+                text === "x" ? text = "o" : text = "x";
+
+                return false; 
+              }
+            }
+          } else {
+            return true;
+          }
+        });
+      };
+
+
+    // const automatic =()=>{
+    //     let randomNumber = Math.floor(Math.random() * 9);
+    //     console.log(randomNumber)
+    // if(block[randomNumber].innerHTML != ' '){
+    //     automatic()
+    // }else{
+    //     block[randomNumber].innerHTML=text;
+    //     for(let c of combination){
+    //         if(checkwin(c)){
+    //             result.innerHTML= text + " is the winner";
+    //             break;
+    //         }
+    //        }
+    // }
+    // }
+
+  
+
+const change=()=>{
+    block.forEach(e=>{
+        e.innerHTML=' '
+    })
+    text = 'x'
+    winner= false
+    result.innerHTML= 'turn for : '+text
+}
+
+reset.addEventListener("click",change)
+
+
+const combination = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+
+
+
+const checkwin =(indexes)=>{
+    let first = block[indexes[0]].innerHTML;
+    if( first != ' ' && indexes.every(i=> block[i].innerHTML === first) ){
+        winner = true;
+        return true
+    }
+}
